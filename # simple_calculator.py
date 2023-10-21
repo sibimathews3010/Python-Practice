52b8419bf05decd72cd86d170c5c7dc3b175a08d
Sibi Mathew <42407386+sibimathews3010@users.noreply.github.com>
2023-10-21 11:24:13 +0530

    Create # simple_calculator.py
    simple calculator

---
+++ # simple_calculator.py
@@ -0,0 +1,19 @@
+# simple calculator
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    result = num1 / num2
+else:
+    print("Invalid operator.")
+    exit()
+
+print("Result:", result)
+
