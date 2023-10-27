e92d130f7ab643049e2aef52a81f77388c3a38e2
Sibi Mathew <42407386+sibimathews3010@users.noreply.github.com>
2023-10-27 10:13:32 +0530

    Create greatest3.py
    Python program to find greatest of three numbers

---
+++ greatest3.py
@@ -0,0 +1,18 @@
+def find_greatest(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+# Get inputs from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+# Call the function to find the greatest among the three numbers
+greatest = find_greatest(num1, num2, num3)
+
+# Print the result
+print("The greatest number is:", greatest)
